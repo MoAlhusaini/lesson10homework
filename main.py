653b7e22041dd90b8d2063067655c6ff68342476
moalhusaini <alhusainimohammad21@gmail.com>
2023-01-30 11:23:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+row1 = -2+(2**2-4*1*-5)**0.5 # i used this to make it easier
+row2 = -2-(2**2-4*1*-5)**0.5
+print("Hello,the roots of quadratic x**2+2x-5 are")
+print(row1/2*1)
+print(row2/2*1)
+print("root1=""1.4494897427831779")
+print("root2=""-3.449489742783178")
+print("Goodbye")
